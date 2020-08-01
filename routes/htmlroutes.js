cc9641e2899e71d2984170b08586430bf967cb6f
Jonayah <jonayah@thinkhalcyon.com>
2020-08-01 00:26:13 -0400

    done with server and html routes

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,20 +1,19 @@
 // Dependencies
 // =============================================================
-const fs = require("fs");
 const path = require("path");
 
 // GET API
 // ===========================================================
 // GET /notes - Should return the notes.html file.
-module.exports = function (app) {
+module.exports = function (app) {  // why doesn't this work when I call the specific name down below?
     app.get("/notes", function(req, res) {
         // locating the file in the static public folder
-        res.sendFile(path.join(__dirname, "public", "notes.html"));
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
     // GET * - Should return the index.html file
     app.get("/*", function(req, res) {
         // locating the file in the static public folder
-        res.sendFile(path.join(__dirname, "public", "index.html"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 };
--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// finding the specific, unchanging file path
+app.use(express.static("./public"));
 
 // require route files
 // ============================================================
