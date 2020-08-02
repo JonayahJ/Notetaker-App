11e53117d26a3292581b9e6e904a4b4a86a45621
Jonayah <jonayah@thinkhalcyon.com>
2020-08-02 13:14:33 -0400

    fixed the post, almost done with delete, added readme to complete

---
+++ README.md
@@ -0,0 +1,39 @@
+# Notetaker App
+  
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  
+  ## Table of Contents
+  1. [Project Description](#description)
+  2. [Installation](#install)
+  3. [Usage](#usage)
+  4. [License](#license)
+  5. [Contributors](#contributors)
+  6. [Tests](#tests)
+  7. [Questions](#questions)
+  
+
+  ## Issues
+I had a number of issues with this assignment.  Mainly, getting the notes to display in the noteList container, overwriting the...
+
+  
+  ## Installation
+  Execute the command "npm install" and install.
+  
+  ## Usage
+  Type your notes into the form in the webpage and click the save button.
+  
+  ### License
+  MIT
+  
+  ### Contributors
+  Jonayah M. Jackson
+  
+  #### Tests
+  No tests
+  
+  #### Questions (FAQs)
+  No questions
+
+  ###### Contact Me
+  * GitHub Profile: https://github.com/JonayahJ
+  * Email Address: jonayah@thinkhalcyon.com
--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,11 +1,10 @@
 // Dependencies
-// =============================================================
 const fs = require("fs");
 const { v4: uuid } = require('uuid')
 const path = require("path");
+const db = require("../db/db.json");
 
 // GET API
-// =============================================================
     // GET /api/notes - Should read the db.json file and return all saved notes as JSON.
 module.exports =  app => {
     // read file
@@ -27,48 +26,50 @@ module.exports =  app => {
 
         })
     );
-};
 
-// POST API
-// =============================================================
-    // POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-
-module.exports =  app => {
-    // send new note
+    // POST API
+     // send new note
     app.post("/api/notes", (req, res) => { 
         // create a new note
-        let newNote = {
-            title: req.body.title,
-            text: req.body.text,
-        };
+        let newNote =
+            {
+                title: req.body.title,
+                text: req.body.text,
+            };
 
         // setting a unique id
         newNote.id = uuid();
+        //db is type array
+        db.push(newNote);
+        // console.log(db);
 
-        console.log("Your note = ", newNote);
-        
-        // sets db file to savedNotes
-        let savedNotes = fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf-8");
-        
-        // pushes the newNote into the savedNotes in db
-        // savedNotes.push(newNote)  // why doesn't .push() work here???
-        
-        // strigifies and writes the newNote into the savedNotes
-        fs.writeFileSync("db/db.json", JSON.stringify(newNote));
-        
-        // returns savedNotes file, can see in POSTman
-        res.json(savedNotes);
-    })
-
+        fs.writeFile("../db/db.json", JSON.stringify(db), function(err) {
+            console.log("done")
+            res.json(db);
+        });
     
-  
+    });
+
+    // DELETE API
+    // delete note
+    app.delete("/api/notes/:id", (req, res) => {
+        //1. look for req.params.id () use a forloop based on length of an array (db)
+        for (let i = 0; i < db.length; i++) 
+        {
+            if(req.params.id === db[i].id)
+            {
+                // console.log(i); 
+                // 2. cut out the index
+                db.splice(i, 1); 
+                //console.log(db)
+
+                //do a .then fs writefile (bc it's not showing up on the page)
+                fs.writeFile("../db/db.json", JSON.stringify(db), function(err) 
+                {
+                    console.log("done")
+                    // returns noteList file, can see in POSTman
+                });
+            }   
+        }
+    });
 };
-
-
-    
-
-
-
-// DELETE API
-// =============================================================
-    // DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
